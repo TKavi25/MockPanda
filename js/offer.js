a970a5d0c43257a11b5a2720e38a119490e50e22
Tahsan <tahsankavi577@gmail.com>
2025-07-26 18:42:33 +0600

    removing comments

--- js/offer.js
+++ js/offer.js
@@ -1,31 +1,3 @@
-// import "./load.js"
-// import "./navBar.js"
-
-
-// const htmlContainer = document.querySelector("main");
-
-// const discountedRestaurants = restaurants.filter(restaurant => restaurant.discount > 0);
-
-// if(discountedRestaurants.length === 0){
-//     htmlContainer.innerHTML = `<h3>No offers available.</h3>`;
-// }else{
-//     discountedRestaurants.forEach(restaurant =>{
-//         htmlContainer.innerHTML +=
-//                 `<div class="restaurant">
-//                     <div class="restaurant-offer">${restaurant.discount}%</div>
-//                     <img src="${restaurant.img}" alt= "restaurant: ${restaurant.name}">
-//                     <div class="restaurant-details">
-//                         <div class="important-restaurant-details">
-//                             <div>${restaurant.name}</div>
-//                             <div>⭐ ${restaurant.rating} <span>(${restaurant.ratingAmount})</span></div>
-//                         </div>
-//                         <div class="more-restaurant-details">🚴🏽 ${restaurant.distance} ◦ ৳${restaurant.deliveryFee}</div>
-//                     </div>
-//                 </div>`
-//     });
-// }
-
-
 import "./load.js"
 import "./navBar.js"
 import { generateRestaurantHtml } from "./generate-restaurantHtml.js";
